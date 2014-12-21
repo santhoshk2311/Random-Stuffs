337937f2dca5ed4a3df94200ae7152f3f0f1d31c
santhosk2311 <santhoshk.2311@gmail.com>
2014-12-20 18:32:55 -0800

    Started with UI.

--- sudoku/js/sudoku.js
+++ sudoku/js/sudoku.js
@@ -1,16 +1,52 @@
+/*
+ *	Sudoku Class.
+ *  Singleton.
+ *  @author santhoshkumar soundararajan
+ */
 var Sudoku = (function($) {
-	var instance;
+	var _instance;
+	var _gameUI;
 
 	function init() {
-		return "game";
+		_gameUI = new SudokuGameUI();
+		return {
+			getBoard: function() {
+				return _gameUI.constructBoardUI();
+			}
+		};
 	}
+
+	/*
+	 *	Sudoku Game UI Class.
+	 *  constructs UI board.
+	 */
+
+	function SudokuGameUI() {
+		// properties  
+	}
+
+	SudokuGameUI.prototype = {
+		constructBoardUI: function() {
+			var table = $('<table></table>').addClass("fit");
+			for(var i=0; i<9; i++){
+			    var row = $('<tr></tr>');
+			    for (var j=0; j<9; j++) {
+			    	row.append($('<td>').html(j));
+			    }
+			    //console.log(row);
+			    table.append(row);
+			}
+			return table;
+		}
+	};
 
 	return {
 		getInstance: function () {
-			if ( !instance ) {
-	    	    instance = init();
+			if (!_instance) {
+	    	    _instance = init();
 	    	}
-    		return instance;
+    		return _instance;
 		}
 	}
+
 }) (jQuery);
