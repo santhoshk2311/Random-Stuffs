6557e82303b264c8ab4f3e701d764e3916ccdb13
santhosk2311 <santhoshk.2311@gmail.com>
2014-12-21 04:38:50 -0800

    more UI changes for board.

--- sudoku/js/sudoku.js
+++ sudoku/js/sudoku.js
@@ -31,8 +31,36 @@ var Sudoku = (function($) {
 			for(var i=0; i<9; i++){
 			    var row = $('<tr></tr>');
 			    for (var j=0; j<9; j++) {
-			    	var input = $('<input type="text"/>');
-			    	row.append($('<td>').append(input));
+			    	var input = $('<input type="text"/>').attr({
+			    		'maxlength': "1",
+			    		'min': '1',
+			    		'max': '9'});
+
+			    	input.keyup(function(){
+			    		var val = $(this).val();
+			    		if (!$.isNumeric(val) || val == 0) {
+			    			this.value = '';
+			    			return false;
+			    		}
+
+			    		if (val.length > 1) {
+			    			this.value = val.substring(0,1);
+			    			console.log(val.substring(0,1));
+			    			return false;
+			    		}
+			    	});
+
+			    	var td = $('<td>');
+
+			    	var blocki = Math.floor(i / 3);
+			    	var blockj = Math.floor(j / 3);
+
+			    	if ((blocki + blockj) % 2 == 0) {
+			    		input = input.addClass("bg-color2");
+					} else {
+						input = input.addClass("bg-color1");
+					}
+			    	row.append(td.append(input));
 			    }
 			    //console.log(row);
 			    table.append(row);
